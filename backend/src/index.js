72b3336cbca9ab081c2ee5fd5f6b887f664ca4d8
lucas.paszinski <lucas.paszinski@altus.com.br>
2020-01-19 19:46:56 -0300

    CHANGED: Index moved to src

---
+++ backend/src/index.js
@@ -0,0 +1,37 @@
+// Importação do Modulo Express
+const express = require( 'express' );
+//Passwords and user manager
+const passwords = require( './passwords.json' );
+// Install Mongoose for MongoDb Connection
+const mongoose = require( 'mongoose' );
+//Constroi o App 
+const app = express();
+
+// Define qual database vai ser usado
+is_local_database = true;
+// Define as opções necessarias ao mongo db
+mongooseOption = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+
+// If is local, get local data base, else online data base
+if ( is_local_database )
+{
+    mongoose.connect( 'mongodb://localhost:27017/Omnistack10', mongooseOption )
+}
+else
+{
+    // Connection string and option for MongoDB Atlas
+    mongoose.connect( `mongodb+srv://${ passwords.mongoDb.user }:${ passwords.mongoDb.password }@omnistack10-finq3.mongodb.net/test?retryWrites=true&w=majority`, mongooseOption );
+}
+
+// Habilita o express a entender json e sempre deve estar antes das
+app.use( express.json() );
+// Importa o module de rotas
+const routes = require( './routes.js' );
+// Habilita as rotas importadas no express
+app.use( routes );
+// Porta utilizada é a 3333
+app.listen( 3333 );
+
