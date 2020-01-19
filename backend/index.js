c364833eeacb2a1192aaebf4340d0feaf0086240
lucas.paszinski <lucas.paszinski@altus.com.br>
2020-01-19 13:22:03 -0300

    ADDED: Example rest api request using Insominia.

--- backend/index.js
+++ backend/index.js
@@ -4,14 +4,55 @@ const express = require( 'express' );
 //Constroi o App 
 const app = express();
 
+///////////////////////////////
+//
 // Hello world  with Express
+//
+// http://localhost:3333/
 app.get( '/', ( request, response ) =>
 {
     // retorna uma resposta em Texto
     //return response.send( 'Eai meu Parceria !' );
+    
     // Retorna uma resposta json
     return response.json( { 'Hello' : 'World!'} );
 } )
+
+
+///////////////////////////////////////////////////////////////
+// Tipos de parâmetros
+// 
+// Query Params: request.query (filtros, ordenação, paginação)
+//
+// http://localhost:3333/user/?hello=world
+app.get( '/user', ( request, response ) =>
+{
+    console.log( request.query )
+    response.json( request.query )
+} )
+
+//////////////////////////////////
+//
+// Route Params: request.params ()
+//
+// http://localhost:3333/user/1
+app.delete( '/user/:id', ( req, res ) =>
+{
+    console.log( req.params.id )
+    res.send(req.params.id)
+} )
+
+/////////////////////////////////////
+//
+// Request.body
+//
+app.post( '/user', ( req, res ) =>
+{
+    const body = req.body;
+    console.log( `Meu nome é ${ body.name } 
+    e email é ${body.email }` )
+    res.json(req.body)
+})
 
 // principas metodos HTTP: GET, POST, PUT e DELETE
 
