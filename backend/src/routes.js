c5100d826b74ac5c1e8ddab363d6b7cdce364d89
Lucas L Paszinski <lucas.paszinski@altus.com.br>
2020-01-20 11:32:51 -0300

    ADDED: Techs and techs to array treatment.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -13,13 +13,17 @@ routes.post( '/devs', async ( req, res ) =>
     github_api_res = await axios.default.get( `https://api.github.com/users/${ github_user }` );
 
     console.log( github_api_res.data ) 
-    // Não funciona o 'name = login' pois o valor não é undefined e sim null
+    
     const { name, login , avatar_url, url, bio } = github_api_res.data;
     
+    // Não funciona o 'name = login' pois o valor não é undefined e sim null 
     const name_login = (!name) ? login : name;
 
-    console.log(name_login, avatar_url, url, bio, String(techs).split(','));
-    res.json( { name_login, avatar_url, url, bio } )
+    // Remove espaços e joga todas as strings para UPPER para padronizar buscas no backend
+    const techArrays = String(techs).toUpperCase().split(",").map(str => str.trim().replace(/\s/g, ''));
+
+    console.log(name_login, avatar_url, url, bio, techArrays);
+    res.json( { name_login, avatar_url, url, bio, techArrays } )
     
 }
 );
