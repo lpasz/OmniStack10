18d424884541388a65b18acafa9d90e307de42e2
Lucas L Paszinski <lucas.paszinski@altus.com.br>
2020-01-22 10:59:23 -0300

    ADDED: Location ( PointSchema )  and it's implmentation in Dev Model

--- backend/src/models/dev.js
+++ backend/src/models/dev.js
@@ -1,12 +1,20 @@
 const mongoose = require( 'mongoose' );
 
+// Utils Schema of user location 
+const PointSchema = require('./utils/PointSchema');
+
+
 const devSchema = mongoose.Schema(
     {
         name: String,
         github_user: String,
         bio: String,
         avatar_url: String,
-        techs: [String]
+        techs: [String],
+        location:{
+            type: PointSchema,
+            i: '2dsphere',
+        },        
     }
 );
 
