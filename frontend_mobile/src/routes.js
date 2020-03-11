e9664237d1138cbeb978bfcab639c5cdab9e57cd
lucas.paszinski <lucas.paszinski@altus.com.br>
2020-03-11 20:56:25 -0300

    CHANGED: Added Map, Routes Stack navigation, Get user permission for location, Get user location.

--- frontend_mobile/src/routes.js
+++ frontend_mobile/src/routes.js
@@ -1,1 +1,30 @@
-  
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack';
+
+import Main from './/pages//main'
+import Profile from './/pages//profile'
+
+const Routes = createAppContainer(
+    createStackNavigator( {
+        DevRadar: {
+            screen: Main,
+            navigationOptions: ()=>({
+                title: 'DevRadar',
+            }),            
+        },
+        Profile: {
+            screen: Profile,
+            navigationOptions: ()=>({
+                title: 'Profile',
+            }),    
+        },
+    }, {
+            defaultNavigationOptions: {
+                headerTintColor: '#FFF',
+                headerStyle: { backgroundColor: '#7D40E7' }
+            } 
+    }
+    )
+)
+
+export default Routes;
